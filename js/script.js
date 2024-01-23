3d7e282c1ce40c28e62dea74279183d2651dcdcb
Muhamad Fahmi Saputra <133094910+FahmiiSptra@users.noreply.github.com>
2024-01-23 16:47:32 +0700

    Update script.js
    menambahkan function di file javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.info("Hallo GIT dan GITHUB saya sedang belajar");
+
+const x = function(firstName, lastName){
+  return `Hallo nama saya ${firstName} ${lastName}`
+}
+
+const y = x("Fahmi", "Saputra")
+console.log(y)
